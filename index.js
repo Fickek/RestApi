a9c7228874ccb203f66ccd881e26b5c8732f0214
Daniil <daniil1758@gmail.com>
2020-12-15 18:01:29 +0600

    added fetch

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const userRouter = require('./routes/user_routes');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-app.use(express.json());
+
 app.use('/', userRouter);
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
+
 
 app.listen(port, () => {
     console.log(`Connected to port: ${port}`);
