87a92dd6590a29c0374501731e1931fdedfb095b
Daniil <daniil1758@gmail.com>
2020-12-16 19:52:39 +0600

    Update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const userRouter = require('./routes/user_routes');
 const bodyParser = require('body-parser');
+const userRouter = require('./routes/user_routes');
+
+const {
+    json
+} = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -13,15 +17,13 @@ app.use(bodyParser.urlencoded({
 
 app.use('/', userRouter);
 
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.post('/', (req, res) => {
+app.post('/', (req, res) => { // req - метод для получения данных от клиента , body - данные полученные от клиента
     console.log(req.body);
 });
 
+app.get('/', (req, res) => { // res - метод для отправки данных клиенту
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(port, () => {
     console.log(`Connected to port: ${port}`);
