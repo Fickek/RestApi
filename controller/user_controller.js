44a006340d8029c91f127b403db875e692f85615
Daniil <daniil1758@gmail.com>
2020-12-20 18:14:38 +0600

    Check mail for unique

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -4,36 +4,48 @@ class UserContoller {
     async createUser(req, res) {
         const {
             name,
-            age
+            mail
         } = req.body;
-        const newPerson = await db.query('INSERT INTO person (name, age) values ($1, $2) RETURNING *', [name, age]);
-        res.json(newPerson.rows[0]);
+
+        let mailUser = await db.query('SELECT mail from person');
+        // console.log(mailUser.rows[0])
+        for (let number in mailUser.rows) { // перебирает каждый элемент по mail и передает его в number
+            // console.log(mailUser.rows[number].mail);
+            if (mailUser.rows[number].mail === mail) {
+                console.log(mail);
+                break;
+            } else {
+                console.log(mail);
+                const newPerson = await db.query('INSERT INTO person (name, mail) values ($1, $2) RETURNING *', [name, mail]);
+                break;
+            }
+        }
     }
-    async getUser(req, res) {
-        const users = await db.query('SELECT * FROM person');
-        res.json(users.rows);
-    }
-    async getOneUser(req, res) {
-        const id = req.params.id;
-        const user = await db.query('SELECT * FROM person where id = $1', [id]);
-        res.json(user.rows[0]);
-    }
-    async updateUser(req, res) {
-        const {
-            id,
-            name,
-            age
-        } = req.body;
-        const user = await db.query(
-            'UPDATE person set name = $1, age = $2 where id = $3 RETURNING *', [name, age, id]
-        );
-        res.json(user.rows[0]);
-    }
-    async deleteUser(req, res) {
-        const id = req.params.id;
-        const user = await db.query('DELETE FROM person where id = $1', [id]);
-        res.json(user.rows[0]);
-    }
+    // async getUser(req, res) {
+    //     const users = await db.query('SELECT * FROM person');
+    //     res.json(users.rows);
+    // }
+    // async getOneUser(req, res) {
+    //     const id = req.params.id;
+    //     const user = await db.query('SELECT * FROM person where id = $1', [id]);
+    //     res.json(user.rows[0]);
+    // }
+    // async updateUser(req, res) {
+    //     const {
+    //         id,
+    //         name,
+    //         age
+    //     } = req.body;
+    //     const user = await db.query(
+    //         'UPDATE person set name = $1, age = $2 where id = $3 RETURNING *', [name, age, id]
+    //     );
+    //     res.json(user.rows[0]);
+    // }
+    // async deleteUser(req, res) {
+    //     const id = req.params.id;
+    //     const user = await db.query('DELETE FROM person where id = $1', [id]);
+    //     res.json(user.rows[0]);
+    // }
 }
 
 module.exports = new UserContoller();
