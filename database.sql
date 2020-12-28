3e11bfacfac53cad201b8e8b0b7341c7897bcdb1
Daniil <daniil1758@gmail.com>
2020-12-28 16:14:15 +0600

    Update database

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
+-- create TABLE person(
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(255),
+--     age INTEGER
+-- );
+
 create TABLE person(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    age INTEGER
-);
-
-create TABLE persons(
-    id SERIAL,
-    name VARCHAR(255),
-    mail INTEGER
+    mail VARCHAR(255),
+    password VARCHAR(255)
 );
